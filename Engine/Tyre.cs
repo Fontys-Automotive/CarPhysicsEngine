a6d831adf084d3e251a471f575275a4cb7b06e65
Ashish Kumar <ashish@ashish.co>
2014-11-06 01:05:50 +0100

    Refactor Tyre.cs

--- Engine/Tyre.cs
+++ Engine/Tyre.cs
@@ -1,93 +1,46 @@
-using System;
-
 namespace CarPhysicsEngine
 {
     public class Tyre
     {
-        private readonly double Cy1; //Tyre stiffness 1-front, 2- rear
-        private readonly double Cy2; //Tyre stiffness 1-front, 2- rear
+        private readonly double _cy1;
+        private readonly double _cy2;
+        private readonly double _forwardVelocity;
+        private readonly double _lengthFront;
+        private readonly double _lengthRear;
 
-        /// <summary>
-        ///     Length from front wheel to centre of gravity
-        /// </summary>
-        private readonly double a;
-
-        /// <summary>
-        ///     Length from rear wheel to centre of gravity
-        /// </summary>
-        private readonly double b;
-
-        private double B1, B2; //stiffness factor
-
-        private double C1, C2; // form factor
-        private double D1, D2; //peak factor
-        private double Fz0, Fz1, Fz2; //F0 - nominal load; F1 - front axle load; F2 - rear axle load
-        private double K1, K2;
-        private double dFz1, dFz2;
-        private double eta = 0.03; //understeering factor. Max 0.04
-        private double mu1, mu2; //friction factor
-
-        /// <summary>
-        ///     Forward velocity
-        ///     !TODO: Pass this value from CarBehaviour
-        /// </summary>
-        private double forwardVelocity; // forward velocity
-
-        public Tyre(double mass, double gravity, double length, double steerAngle, double yawVelocity,
-            double lateralVelocity, double a, double b, double Fz0, double forwardVelocity)
+        public Tyre(double lengthRear, double lengthFront, double forwardVelocity, double cy1, double cy2)
         {
-            this.forwardVelocity = forwardVelocity;
-            SteerAngle = steerAngle;
-            YawVelocity = yawVelocity;
-            LateralVelocity = lateralVelocity;
-            this.a = a;
-            this.b = b;
-            C1 = C2 = 1.19;
-            this.Fz0 = Fz0;
-            Fz1 = (mass*gravity*b)/length;
-            Fz2 = (mass*gravity*a)/length;
-            dFz1 = (Fz1 - Fz0)/Fz0;
-            dFz2 = (Fz2 - Fz0)/Fz0;
-            mu1 = mu2 = 0.9;
-            D1 = mu1*(-0.145*dFz1 + 0.99)*Fz1;
-            D2 = mu2*(-0.145*dFz2 + 0.99)*Fz2;
-            K1 = 14.95*this.Fz0*Math.Sin(2*Math.Atan(Fz1/2.13/this.Fz0));
-            K2 = Fz2*K1/(Fz1 - eta*K1);
-            B1 = K1/C1/D1;
-            B2 = K2/C2/D2;
-            Cy1 = B1*C1*D1;
-            Cy2 = B2*C2*D2;
+            _lengthRear = lengthRear;
+            _lengthFront = lengthFront;
+            _forwardVelocity = forwardVelocity;
+            _cy1 = cy1;
+            _cy2 = cy2;
         }
 
+        public double SteerAngle { get; set; }
         public double YawVelocity { get; set; }
         public double LateralVelocity { get; set; }
-        public double SteerAngle { get; set; }
 
-        //TYRE FORCE
-        public double tyreForceFront()
+        public double TyreForceFront()
         {
-            return alphaFront() * Cy1;
+            return AlphaFront() * _cy1;
         }
 
-        public double tyreForceRear()
+        public double TyreForceRear()
         {
-            return alphaRear() * Cy2;
+            return AlphaRear() * _cy2;
         }
 
-        // SLIP ANGLE
-        private double alphaFront()
+        private double AlphaFront()
         {
-            var n1 = (YawVelocity*a) + LateralVelocity;
-            var n2 = n1/forwardVelocity;
-            return SteerAngle - n2;
+            var n1 = (YawVelocity * _lengthFront + LateralVelocity) / _forwardVelocity;
+
+            return SteerAngle - n1;
         }
 
-        private double alphaRear()
+        private double AlphaRear()
         {
-            var n1 = (LateralVelocity) - (YawVelocity * b);
-            var n2 = -n1/forwardVelocity;
-
-            return n2;
+            return (YawVelocity * _lengthRear - LateralVelocity) / _forwardVelocity;
         }
     }
 }
