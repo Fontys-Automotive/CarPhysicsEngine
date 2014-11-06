c29032faa6832b6d9c1b3df2505fb7ae34d406e4
Ashish Kumar <ashish@ashish.co>
2014-11-06 01:04:18 +0100

    Add pre-defined input to CarBehaviour instead of Tyre

--- Engine/CarBehaviour.cs
+++ Engine/CarBehaviour.cs
@@ -4,25 +4,78 @@ namespace CarPhysicsEngine
 {
     public class CarBehaviour
     {
-        /// <summary>
-        ///     Acceleration due to gravity
-        /// </summary>
-        private static double gravity;
+        // Pre-defined variables from voertuigdata.m
+        private readonly double mass;// vehicle mass [kg]
+        private readonly double gravity; // acceleration due to gravity [m/s^2]
+        private readonly double lengthWheelbase; // wheelbase length [m]
+        private readonly double lengthFront; // distance from front axle to centre [m]
+        private readonly double lengthRear; // distance from rear axle to centre [m]
+        private readonly double inertiaMoment; // moment of inertia of vehicle on z-axis [kgm^2]
+        private readonly double ETA; // understeer factor. Plausible max value is 0.04
+        private readonly double Fz0; // Nominal load [N]
+        private readonly double Fz1; // Axle load front [N]
+        private readonly double Fz2; // Axle load read [N]
+        private readonly double dFz1;
+        private readonly double dFz2;
+        private readonly double mu1 = 0.9; // coefficient of friction front [-]
+        private readonly double mu2 = 0.9; // coefficient of friction rear [-]
+        private readonly double D1; // Peak-factor front [N]
+        private readonly double D2; // Peak-factor rear [N]
+        private readonly double C1 = 1.19; // "vormfactor" front [-]
+        private readonly double C2 = 1.19; // "vormfactor" rear [-]
+        private readonly double K1;
+        private readonly double K2;
+        private readonly double B1; // Stiffness factor front
+        private readonly double B2; // Stiffness factor rear
+        private readonly double E1; // curvature factor front
+        private readonly double E2; // curvature factor rear
+        private readonly double Cy1; // Tyre stiffness front [N/rad]
+        private readonly double Cy2; // Tyre stiffness rear [N/rad]
+        private readonly double forwardVelocity; // [m/s]
+        private readonly double yawFactor; // steering factor [deg]
 
-        public readonly Forces forces;
-        public Position position;
-        /// <summary>
-        ///     Forward Velocity for current iteration
-        /// </summary>
-        public readonly double forwardVelocity;
+        private Tyre tyre;
+        private Forces forces;
+        private Movement movement;
+        private Position position;
+
+        public CarBehaviour()
+        {
+            // Pre-defined input from voertuigdata.m
+            mass = 1150;
+            gravity = 9.81;
+            lengthWheelbase = 2.66;
+            lengthFront = 1.06;
+            lengthRear = lengthWheelbase - lengthFront;
+            inertiaMoment = 1850;
+            ETA = 0.03;
+            Fz0 = 8000;
+            Fz1 = mass * gravity * lengthRear / lengthWheelbase;
+            Fz2 = mass * gravity * lengthFront / lengthWheelbase;
+            dFz1 = (Fz1 - Fz0) / Fz0;
+            dFz2 = (Fz2 - Fz0) / Fz0;
+            mu1 = 0.9;
+            mu2 = 0.9;
+            D1 = mu1 * (-0.145 * dFz1 + 0.99) * Fz1;
+            D2 = mu2 * (-0.145 * dFz2 + 0.99) * Fz2;
+            C1 = 1.19;
+            C2 = 1.19;
+            K1 = 14.95 * Fz0 * Math.Sin(2 * Math.Atan(Fz1 / 2.13 / Fz0));
+            K2 = Fz2 * K1 / (Fz1 - ETA - K1);
+            B1 = K1 / C1 / D1;
+            B2 = K2 / C2 / D2;
+            E1 = -1.003 - 0.537 * dFz1;
+            E2 = -1.003 - 0.537 * dFz2;
+            Cy1 = B1 * C1 * D1;
+            Cy2 = B2 * C2 * D2;
+            forwardVelocity = 80 / 3.6;
+            yawFactor = 2;
+        }
 
         /// <summary>
         ///     Lateral velocity for current iteration
         /// </summary>
         private readonly double lateralVelocity;
-
-        public readonly Movement movement;
-        public readonly Tyre tyre;
 
         /// <summary>
         ///     Time step for current interation (for use with integration)
@@ -87,7 +140,7 @@ namespace CarPhysicsEngine
         /// </summary>
         public double yCoordinate { get; private set; }
 
-        public CarBehaviour()
+        public void CarBehaviourOLDCONSTRUCTOR()
         {
             SteerAngleRadians = 0.00;
 
@@ -128,7 +181,7 @@ namespace CarPhysicsEngine
 
             // Object creation initialization
             tyre = new Tyre(mass, gravity, length, SteerAngleRadians, yawVelocityRadians, lateralVelocity, a, b, fz0, forwardVelocity);
-            forces = new Forces(tyre.tyreForceFront(), tyre.tyreForceRear(), a, b);
+            forces = new Forces(tyre.TyreForceFront(), tyre.TyreForceRear(), a, b);
             movement = new Movement(forces.FyTotal(), forces.MzMoment(), I, mass, previousFyTotal, previousMzTotal, dt,
                 forwardVelocity, previousYawVelocity, previousLateralVelocity);
             position = new Position(forwardVelocity, previousForwardVelocity, yawVelocity(), 
@@ -173,8 +226,8 @@ namespace CarPhysicsEngine
             tyre.YawVelocity = movement.yawVelocity();
             tyre.LateralVelocity = movement.lateralVelocity();
 
-            forces.TyreForceFront = tyre.tyreForceFront();
-            forces.TyreForceRear = tyre.tyreForceRear();
+            forces.TyreForceFront = tyre.TyreForceFront();
+            forces.TyreForceRear = tyre.TyreForceRear();
 
             movement.CurrentFyTotal = forces.FyTotal();
             movement.CurrentMzTotal = forces.MzMoment();
