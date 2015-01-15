31846ed9fe9761c95608fd9a9ca6cc506ff80c4e
Ashish Kumar <ashish@ashish.co>
2015-01-15 13:11:51 +0100

    Delete BoundryFriction.cs

--- Engine/Acceleration/VehicleModel.cs
+++ Engine/Acceleration/VehicleModel.cs
@@ -2,6 +2,14 @@
 {
     public class VehicleModel
     {
-         
+        private static double PositiveFriction
+        {
+            get { return Setup.Cw * Setup.M * Setup.G; }
+        }
+
+        private static double NegativeFriction
+        {
+            get { return PositiveFriction * -1; }
+        }
     }
 }
