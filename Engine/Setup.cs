094eef8b06883106a3cf89349b17ee8ca7106e81
Ashish Kumar <ashish@ashish.co>
2015-01-15 13:00:40 +0100

    Use static for variables in Setup.cs

--- Engine/Setup.cs
+++ Engine/Setup.cs
@@ -4,19 +4,19 @@ namespace CarPhysicsEngine
 {
     public class Setup
     {
-        public readonly double Cw = 0.3; // C2-waarde [-]
-        public readonly double Rho = 1.29; // Air Density [kg/m^3]
-        public readonly double A = 2.25; // Frontal Area [m^2]
-        public readonly double M = 950; // Vehicle Mass [kg]
-        public readonly double G = 9.81; // Acceleration due to Gravity [m/s^2]
-        public readonly double Fr = 0.014; // Rolling Resistance Coefficient [-]
-        public readonly double MuMax = 1.05; // Maximum Friction Coefficient [-]
-        public readonly double R = 0.3; // Tire Radius [m]
-        public readonly double J1 = 1.2; // Traagheid ververbrandingsmotor [kgm^2]
-        public readonly double J2 = 5.4; // Traagheid wielen en aandrijving [kgm^2]
-        public readonly double SimulationTime = 1180; // [s]
+        public static readonly double Cw = 0.3; // C2-waarde [-]
+        public static readonly double Rho = 1.29; // Air Density [kg/m^3]
+        public static readonly double A = 2.25; // Frontal Area [m^2]
+        public static readonly double M = 950; // Vehicle Mass [kg]
+        public static readonly double G = 9.81; // Acceleration due to Gravity [m/s^2]
+        public static readonly double Fr = 0.014; // Rolling Resistance Coefficient [-]
+        public static readonly double MuMax = 1.05; // Maximum Friction Coefficient [-]
+        public static readonly double R = 0.3; // Tire Radius [m]
+        public static readonly double J1 = 1.2; // Traagheid ververbrandingsmotor [kgm^2]
+        public static readonly double J2 = 5.4; // Traagheid wielen en aandrijving [kgm^2]
+        public static readonly double SimulationTime = 1180; // [s]
 
-        public readonly Dictionary<double, double> GearRatio = new Dictionary<double, double>()
+        public static readonly Dictionary<double, double> GearRatio = new Dictionary<double, double>()
         {
             {1, 12},
             {2, 7.5},
@@ -25,7 +25,7 @@ namespace CarPhysicsEngine
             {5, 3.75}
         };
 
-        public readonly Dictionary<double, double> SwitchingBehaviour = new Dictionary<double, double>()
+        public static readonly Dictionary<double, double> SwitchingBehaviour = new Dictionary<double, double>()
         {
             {1, 0},
             {2, 4.17},
@@ -34,7 +34,7 @@ namespace CarPhysicsEngine
             {5, 19.44}
         };
 
-        public readonly Dictionary<double, double> EngineTorque = new Dictionary<double, double>()
+        public static readonly Dictionary<double, double> EngineTorque = new Dictionary<double, double>()
         {
             {0, 50},
             {1000, 90},
