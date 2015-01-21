e88863b4bec1b1f4fe188dbf473a677b14f15b90
Ashish Kumar <ashish@ashish.co>
2015-01-21 21:27:19 +0100

    Add documentation for Helpers.SaturationDynamic

--- Engine/Helpers.cs
+++ Engine/Helpers.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CarPhysicsEngine
+﻿namespace CarPhysicsEngine
 {
     public static class Helpers
     {
+        /// <summary>
+        ///     MATLAB Saturation Dynamic Function
+        ///     
+        ///     Ensures input is within upper and lower bounds
+        /// </summary>
+        /// <param name="lowerBoundary">Minimum possible value</param>
+        /// <param name="upperBoundary">Maximum possible value</param>
+        /// <param name="input">Input to normalize</param>
+        /// <returns>Normalized Input</returns>
         public static double SaturationDynamic(double lowerBoundary, double upperBoundary, double input)
         {
             if (input > upperBoundary)
